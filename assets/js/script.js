a7bfb839f0e6911ff315400ea00b9ca25e879088
Tatyana1502 <tatyana15022011@gmail.com>
2023-10-27 12:47:07 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,72 @@
 let allPokemons;
 let allElements;
+let saisies = [];
 const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/limit/25";
-const urlApiElements = "https://pokebuildapi.fr/api/v1/pokemon/limit/25";
-// const urlApiElements = "https://pokebuildapi.fr/api/v1/types";
-
-
-let radio = document.querySelector("input[type=radio]:checked");
-console.log(radio.value);
-let radioNom;
-radioNom = document.getElementById('radioNom');
-// if (contact=radioNom) {
-await listPokemons ();
-     async function listPokemons () {
-          const res = await fetch(urlApi);
-         allPokemons = await res.json();
-     }
-    console.log(allPokemons);
-    allPokemons.forEach(pokemon => {
-         let monOption = document.createElement("option");
-         //  monOption.value = pokemon.name;
-         monOption.innerText = pokemon.name;
-         document.querySelector('select').appendChild(monOption);       
-     })
-     document.querySelector("select").addEventListener("change", (optionChange) => {
-         console.log("select:", optionChange.target.value);
-         let pokemontrouve = allPokemons.find((element) => element.name == optionChange.target.value);
-         console.log("image:", pokemontrouve.image);
-         document.querySelector(".pokemon-img").setAttribute("src", pokemontrouve.image);
-     })
-// }
+const urlApiElements = "https://pokebuildapi.fr/api/v1/types";
 let radioElement;
 radioElement = document.getElementById('radioElement');
-// if (contact=radioElement) {
-await listElements ();
+// saisies = [];  
+document.querySelectorAll("input[type='radio']").forEach(async radio => {
+    radio.addEventListener("change", async () => {
+        document.querySelector('select').innerHTML = "";
+        if (radio.value == "nom") {
+        await listPokemons ();
+    } else {
+        await listElements ();
+    }
+    })    
+// });
+await listPokemons ();
+     async function listPokemons () {
+         const res = await fetch(urlApi);
+         allPokemons = await res.json();
+         console.log(allPokemons);
+         allPokemons.forEach(pokemon => {
+             let monOption = document.createElement("option");
+             monOption.value = pokemon.name;
+             monOption.innerText = pokemon.name;
+             document.querySelector('select').appendChild(monOption);
+                    
+         });
+         document.querySelector("select").addEventListener("change", (optionChange) => {
+             console.log("select:", optionChange.target.value);
+                 let pokemontrouve = allPokemons.find((element) => element.name == optionChange.target.value);
+             console.log("image:", pokemontrouve.image);
+                 document.querySelector(".pokemon-img").setAttribute("src", pokemontrouve.image);
+             console.log("stats:", pokemontrouve.stats); 
+                 document.querySelector(".pokemon-stats").innerHTML= "HP : " + pokemontrouve.stats.HP;
+                 document.querySelector(".pokemon-stats1").innerHTML= "attack : " + pokemontrouve.stats.attack;
+                 document.querySelector(".pokemon-stats2").innerHTML= "defense : " + pokemontrouve.stats.defense; 
+                 document.querySelector(".pokemon-stats3").innerHTML= "special_attack : " + pokemontrouve.stats.special_attack;
+                 document.querySelector(".pokemon-stats4").innerHTML= "special_defense : " + pokemontrouve.stats.defense;
+                 document.querySelector(".pokemon-stats5").innerHTML= "speed : " + pokemontrouve.stats.speed; 
+                   
+        })
+    }
+await listElements (); 
 async function listElements () {
     const res = await fetch(urlApiElements);
     allElements = await res.json();
-}
     console.log(allElements);
-    allElements.forEach(pokemon => {
-          let monOption = document.createElement("option");
-          // monOption.value = pokemon.stats.HP;
-          monOption.innerText = pokemon.name;
-         document.querySelector('#pet-select2').appendChild(monOption);       
-   })
+    allElements.forEach(element => {
+        let monOption = document.createElement("option");
+        monOption.value = element.name;
+        monOption.innerText = element.name;
+       document.querySelector('select').appendChild(monOption);       
+ })
     document.querySelector("select").addEventListener("change", (optionChange) => {
-         console.log("select:", optionChange.target.value);
-         let pokemonstats= allPokemons.find((element) => element.name == optionChange.target.value);
-         console.log("stats:", pokemonstats.stats);
-        //  document.querySelector(".pokemon-stats").setAttribute("innerText", pokemonstats.stats);
-         document.documentElement.textContent;
-        //  pokemonstats.stats.innerText = pokemon.stats;
-     })
-// }
+    console.log("select:", optionChange.target.value);
+    let pokemontrouve = allElements.find((element) => element.name == optionChange.target.value);
+    console.log("image:", pokemontrouve.image);
+    document.querySelector(".pokemon-img").setAttribute("src", pokemontrouve.image);
+  
+})
+    // console.log(allElements);
+    
+}
+
+});
+
 
 
 
